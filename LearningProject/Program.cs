2d58450a0c9c2feffd1609e738cfc093f86efac1
CalebA1243 <49262753+CalebA1243@users.noreply.github.com>
2019-08-19 19:26:00 -0400

    Added basic tests

---
+++ LearningProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace LearningProject
+{
+
+    public abstract class Test
+    {
+        public abstract void TestMethod();
+
+        public void TestClassMethod()
+        {
+            Console.Write("The abstract class method got called.");
+        }
+    }
+
+    public interface ITest
+    {
+        void Test();
+    }
+
+    public class TestImplementation : Test, ITest
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TestMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImplemenation = new Test();
+        }
+    }
+}
